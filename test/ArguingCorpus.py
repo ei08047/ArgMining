6773c5489fe54adab4d545bc19b5dc04a969a123
Zé <ei08047@fe.up.pt>
2017-10-20 02:54:21 +0100

    ups i need git ignoregit add -A!

--- test/ArguingCorpus.py
+++ test/ArguingCorpus.py
@@ -1,5 +1,9 @@
 import Config
 from Corpus import Corpus
+import re
+import os
+import shutil
+from nltk.corpus.reader import XMLCorpusReader,XMLCorpusView
 
 
 doclist_healthcare_train_anti='doclist_healthcare_train_anti.txt'
@@ -22,22 +26,170 @@ config = Config.Config(['arguing_corpus'])
 config.run()
 
 print('reading arguing_corpus corpus')
-arguing_corpus = Corpus(config.data_path, config.getCorpusPath(config.corpusList[0]), '/training')
+corpus_path = config.getCorpusPath(config.corpusList[0])
+arguing_corpus = Corpus(config.data_path,corpus_path , '/training')
+##arguing_corpus.view()
+
+##prepare file structure to feed opinion finder
+if(False):
+    a_c = 'arguing_corpus'
+    a_c_d = a_c + '/doc_list'
+
+    doc_list = []
+    if (os.path.exists(a_c) and os.path.exists(os.path.exists(a_c))):
+        print('arguing_corpus and doc list exists')
+        print(os.path.exists(arguing_corpus.path))
+        for doc in arguing_corpus.all_list:
+            if(os.path.exists( arguing_corpus.path + '/'+doc)):
+                folder_name = doc.split('/')[1].replace('.txt','')
+                doc_list.append(folder_name)
+                print(folder_name, ' file exists')
+                os.mkdir('arguing_corpus'+'/'+folder_name)
+                #copy for
+                shutil.copy(arguing_corpus.path + '/'+doc, 'arguing_corpus'+'/'+folder_name)
+            else:
+                print('not found',doc)
+        with open(a_c_d,'a') as file:
+            for doc in doc_list:
+                file.write('database/docs/arguing_corpus/'+doc + '/' + doc + '.txt' +'\n')
+    else:
+        print('no')
+
+
+## read arguing corpus annotations
+    ##span start end
+        ## <Annotation Id="13" Type="arg" StartNode="55" EndNode="104">
+xmlCorpusReader = XMLCorpusReader(arguing_corpus.path,  'training/.*xml')
+OC_docs = xmlCorpusReader.fileids()
+
+
+arg_annotations = {}  ## key = documentId : value = [Arg_Span]
+
+class Span:
+    def __init__(self,start,end):
+        self.start=start
+        self.end=end
+    def match(self,other):
+        if(self.start < other.start and self.end < other.start):
+            return -1
+        elif (self.start > other.start and other.end < self.start):
+            return -1
+        else:
+            return 1
 
 
 
-arguing_corpus.view()
+class Arg(Span):
+    def __init__(self,id,span):
+        self.id=id
+        super(Arg,self).__init__(span.start,span.end)
+
+class Sub(Span):
+    def __init__(self,span):
+        super(Sub,self).__init__(span.start,span.end)
+class Obj(Span):
+    def __init__(self,span):
+        super(Obj,self).__init__(span.start,span.end)
+
+#read arg annotations
+all_arg_dict={}
+for id_doc in OC_docs:
+    argList = []
+    x = XMLCorpusView(arguing_corpus.path+'/'+id_doc,'GateDocument/AnnotationSet/Annotation.*')
+    args = [a for a in x if a.attrib['Type']== 'arg']
+    arg_annotations[id_doc] = args
+    for ann in args:
+        a = Arg(ann.attrib['Id'],Span(int(ann.attrib['StartNode']),int(ann.attrib['EndNode'])))
+        argList.append(a)
+
+    p = re.compile('training/(.{5}).xml$')
+    id=p.match(id_doc).group(1)
+
+    all_arg_dict[id] = argList
+
+print(all_arg_dict)
+
+## read opinion finder output
+    ## sentence label(obj/sub) start end
+    ## hc084_hc084.txt_1725_1825	obj
+results_dict = {}
+sub_obj_dict = {'obj':Obj , 'subj':Sub}
+for id_doc in OC_docs:
+    p = re.compile('training/(.{5}).xml$')
+    id=p.match(id_doc).group(1)
+    opinion_finder_result = 'opinionfinderv2.0/database/docs/arguing_corpus/' +id + '/' + id + '.txt_auto_anns/sent_subj.txt'
+    line_pattern = re.compile('.{5}_.{5}.txt_(.{1,5})_(.{1,5})..(obj|subj)\n')
+    ann_list=[]
+    with open(opinion_finder_result) as file:
+        content = file.readlines()
+        for x in content:
+            try:
+                #print(line_pattern.findall(x).pop())
+                (start,end,sub_obj) = line_pattern.findall(x).pop()
+                ann = sub_obj_dict[sub_obj](Span(int(start),int(end)))
+                ann_list.append(ann)
+            except ValueError:
+                print('ValueError', x)
+            except IndexError:
+                print('IndexError', x)
+            finally:
+                #print(start,end,sub_obj)
+                pass
+        #line_pattern.match(x).group(1)
+    results_dict[id]= ann_list
+print(results_dict)
+
+sub_results_dict={}
+obj_results_dict={}
+for id in results_dict.keys():
+    anns = results_dict[id]
+    sub_results_dict[id] = [sub_anns for sub_anns in anns if type(sub_anns).__name__=='Sub' ]
+    obj_results_dict[id] = [obj_anns for obj_anns in anns if type(obj_anns).__name__=='Obj' ]
+    #print(id, 'sub:',len(sub_results_dict[id]),'obj:',len(obj_results_dict[id]))
+
+num_sub_matches=0
+num_obj_matches=0
+num_sub=0
+num_obj=0
+num_arg=0
+
+for id in all_arg_dict:
+    num_sub_matches_per_document = 0
+    num_obj_matches_per_document = 0
+    sub_nodes = sub_results_dict[id]
+    obj_nodes = obj_results_dict[id]
+    arg_nodes = all_arg_dict[id]
+    for arg in arg_nodes:
+        for sub in sub_nodes:
+            if(arg.match(sub)!= -1 ):
+                num_sub_matches_per_document = num_sub_matches_per_document + 1
+    for arg in arg_nodes:
+        for obj in obj_nodes:
+            if(arg.match(obj)!= -1 ):
+                num_obj_matches_per_document = num_obj_matches_per_document + 1
 
 
+    num_sub_sentences_per_document = len(sub_nodes)
+    num_obj_sentences_per_document = len(obj_nodes)
+    num_arg_spans_per_document = len(arg_nodes)
 
+    num_sub_matches = num_sub_matches + num_sub_matches_per_document
+    num_obj_matches = num_obj_matches + num_obj_matches_per_document
+    num_arg = num_arg + num_arg_spans_per_document
+    num_sub = num_sub + num_sub_sentences_per_document
+    num_obj = num_obj + num_obj_sentences_per_document
 
+    print('docId:', id, ' ##num Sentences:', num_sub_sentences_per_document + num_obj_sentences_per_document, ' ##num Args:', num_arg_spans_per_document)
+    print(' ##       num Sub Matches', num_sub_matches_per_document, '##num Sub Sentences:', num_sub_sentences_per_document)
+    print(' ##       num Obj Matches', num_obj_matches_per_document, '##num Obj Sentences:', num_obj_sentences_per_document)
+    print('#ObjArg/#Obj : ', round (float(num_obj_matches_per_document / num_obj_sentences_per_document), 4), '#SubArg/#Sub : ', round(float(num_sub_matches_per_document / num_sub_sentences_per_document), 4))
 
+print('\n\n\n', 'Totals: \n')
 
+print(' ##num Sentences:', num_sub + num_obj,
+      ' ##num Args:', num_arg)
+print(' ##       num Sub Matches', num_sub_matches, '##num Sub Sentences:', num_sub)
+print(' ##       num Obj Matches', num_obj_matches, '##num Obj Sentences:', num_obj)
+print('#ObjArg/#Obj : ', round(float(num_obj_matches / num_obj), 4),
+      '#SubArg/#Sub : ', round(float(num_sub_matches / num_sub), 4))
 
-
-
-
-
-#config.run()
-#a = ArguingCorpus(config)
-#a.read()
--- test/MPQA.py
+++ test/MPQA.py
@@ -27,7 +27,6 @@ class Agent(Annotation):
         self.nested_source = nested_source
         self.agent_uncertain = agent_uncertain
 
-
 # expressive-subjectivity annotation
 # es_uncertain
 class SE(Annotation):
@@ -41,21 +40,30 @@ class SE(Annotation):
         self.nested_source_uncertain = nested_source_uncertain
         self.intensity = intensity
 
-
 class TargetFrame(Annotation):
-    def __init__(self,annotation_id,span):
+    def __init__(self,annotation_id,span,id,sTarget_link,newETarget_link):
         #print('called TargetFrame constructor')
         Annotation.__init__(self,annotation_id,span)
+        self.id = id
+        self.sTarget_link = sTarget_link
+        self.newETarget_link = newETarget_link
 
 class ETarget(Annotation):
-    def __init__(self, annotation_id, span):
+    def __init__(self, annotation_id, span, id ,type, isNegated,isReferredInSpan):
         #print('called ETarget constructor')
         Annotation.__init__(self, annotation_id, span)
+        self.id = id
+        self.type= type
+        self.isNegated = isNegated
+        self.isReferredInSpan = isReferredInSpan
 
 class STarget(Annotation):
-        def __init__(self, annotation_id, span):
-            print('called STarget constructor')
+        def __init__(self, annotation_id, span, id , target_uncertain, eTarget_link):
+            #print('called STarget constructor')
             Annotation.__init__(self, annotation_id, span)
+            self.id = id
+            self.target_uncertain = target_uncertain
+            self.eTarget_link = eTarget_link
 
 # direct-subjective annotation
 # Marks direct mentions of private states and speech events (spoken or written) expressing private states.
@@ -63,8 +71,18 @@ class DS(Annotation):
     def __init__(self,annotation_id,span , id, nested_source, atitude_link, annotation_uncertain=None, implicit=None,
                  subjective_uncertain=None, intensity=None, expression_intensity=None, polarity=None,
                  insubstancial=None):
-        #print('called DS constructor')
+        print('called DS constructor')
         Annotation.__init__(self,annotation_id,span)
+        self.id = id
+        self.nested_source = nested_source
+        self.atitude_link = atitude_link
+        self.annotation_uncertain = annotation_uncertain
+        self.implicit = implicit
+        self.subjective_uncertain = subjective_uncertain
+        self.intensity = intensity
+        self.expression_intensity = expression_intensity
+        self.polarity = polarity
+        self.insubstancial= insubstancial
 
 class Sentence(Annotation):
     def __init__(self,annotation_id,span):
@@ -74,12 +92,17 @@ class Sentence(Annotation):
 # objective-speech-event annotation
 # Marks speech events that do not express private states.
 class OSE:
-    def __init__(self,annotation_id, span, id, nested_source, targetFrame, annotation_uncertain=None, implicit='None',
+    def __init__(self,annotation_id, span, id, nested_source, targetFrame, annotation_uncertain=None, implicit=None,
                  objective_uncertain=None, insubstantial=None):
         #print('called objective-speech-event constructor')
         Annotation.__init__(self,annotation_id,span)
         self.id = id
-
+        self.neste_source = nested_source
+        self.targetFrame = targetFrame
+        self.annotation_uncertain = annotation_uncertain
+        self.implicit = implicit
+        self.objective_uncertain = objective_uncertain
+        self.insubstantial = insubstantial
 
 # Marks the attitudes that compose the expressed private states.
 
@@ -107,10 +130,20 @@ class Document:
     def __init__(self, parent, docleaf):
         self.parent = parent
         self.docleaf = docleaf
+        self.annotation_list = []
     def get_doc_leaf(self):
         return self.docleaf
     def getPath(self):
         return str(self.parent + '/' + self.docleaf)
+    def addAnnotation(self,anno ):
+        self.annotation_list.append(anno)
+
+    def view(self):
+        print('parent:',self.parent, '##doc_leaf:', self.docleaf , '##num_anno:', len(self.annotation_list))
+        print('num attitudes:',len([att for att in self.annotation_list if type(att).__name__=='Attitude']))
+        print('num sub: ', len([ds for ds in self.annotation_list if type(ds).__name__ == 'DS']))
+        print('num obj: ', len([ose for ose in self.annotation_list if type(ose).__name__ == 'OSE']))
+        print('num expressive-subjectivity: ', len([es for es in self.annotation_list if type(es).__name__ == 'SE']))
 
 class MPQA:
     name = 'mpqa'
@@ -121,11 +154,26 @@ class MPQA:
     doclist = 'doclist'
 
     def __init__(self, dataPath, corpusPath):
+
         self.dataPath = dataPath
         self.corpusPath = corpusPath
         self.path = str(self.dataPath + self.corpusPath)
 
         # parse doc list
+        self.doc_list = self.parse_doc_list()
+
+        #parse meta data
+        for meta_doc in self.doc_list:
+            self.parse_meta(meta_doc)
+
+        #parse annotations
+        for anns_doc in self.doc_list:
+            self.parse_anns(anns_doc)
+
+        for doc in self.doc_list:
+            doc.view()
+
+    def parse_doc_list(self):
         temp = []
         doc_list_path = str(self.path + '/doclist')
         text_file = open(doc_list_path, "r", encoding='utf8')
@@ -133,55 +181,46 @@ class MPQA:
             rows = csv.reader(infile, delimiter='\n')
             for row in rows:
                 temp.append(row.pop())
-        self.doc_list = [Document(doc.split('/')[0], doc.split('/')[1]) for doc in temp]
+        return [Document(doc.split('/')[0], doc.split('/')[1]) for doc in temp]
 
-        self.parse_meta()
-        self.parse_anns()
+    def parse_text(self):
+        print('parsing text')
 
-
-
-    def parse_meta(self):
-        for meta_doc in self.doc_list:
-            doc_path = meta_doc.getPath()
-            full_path = self.getFullPath(MPQA.meta_anns, doc_path)
-            if (not os.path.exists(full_path)):
-                print('full_path: ', full_path, os.path.exists(full_path))
-            else:
-                try:
-                    text_file = open(full_path, "r", encoding='utf8')
-                    with text_file as infile:
-                        rows = csv.reader(infile, delimiter='\t')
-                        for row in rows:
-                            encoded_row = [x.encode('utf-8').decode() for x in row]
-                            if ('meta_title' in encoded_row):
-                                meta_title =encoded_row[4]
-                except UnicodeDecodeError:
-                    print('?????')
-
-                    # for meta in self.meta_anns:
-                    #   m = Meta(meta)
-
-    def parse_anns(self):
-        comment_pattern = re.compile("^#")
-        for anns_doc in self.doc_list:
-            doc_path = anns_doc.getPath()
-            full_path = self.getFullPath(MPQA.man_anns, doc_path)
-            path_to_annotation = str(full_path + '/gateman.mpqa.lre.3.0')
-            if (not os.path.exists(path_to_annotation)):
-                print('???')
-            else:
-                print('full_path: ',full_path)
-                annotation_file = open(path_to_annotation, "r", encoding='utf-8')
-                with annotation_file as infile:
-                    rows = csv.reader(infile, delimiter='\n')
+    def parse_meta(self, doc):
+        doc_path = doc.getPath()
+        full_path = self.getFullPath(MPQA.meta_anns, doc_path)
+        if (not os.path.exists(full_path)):
+            print('full_path: ', full_path, os.path.exists(full_path))
+        else:
+            try:
+                text_file = open(full_path, "r", encoding='utf8')
+                with text_file as infile:
+                    rows = csv.reader(infile, delimiter='\t')
                     for row in rows:
-                        line = row.pop()
-                        if(comment_pattern.match(line)):
-                            pass
-                        else:
-                            self.parse_annotation_line(line)
+                        encoded_row = [x.encode('utf-8').decode() for x in row]
+                        if ('meta_title' in encoded_row):
+                            meta_title =encoded_row[4]
+            except UnicodeDecodeError:
+                print('?????')
 
-
+    def parse_anns(self, doc):
+        comment_pattern = re.compile("^#")
+        doc_path = doc.getPath()
+        full_path = self.getFullPath(MPQA.man_anns, doc_path)
+        path_to_annotation = str(full_path + '/gateman.mpqa.lre.3.0')
+        if (not os.path.exists(path_to_annotation)):
+            print('???')
+        else:
+            #print('full_path: ',full_path)
+            annotation_file = open(path_to_annotation, "r", encoding='utf-8')
+            with annotation_file as infile:
+                rows = csv.reader(infile, delimiter='\n')
+                for row in rows:
+                    line = row.pop()
+                    if(comment_pattern.match(line)):
+                        pass
+                    else:
+                        doc.addAnnotation(self.parse_annotation_line(line))
 
     def parse_annotation_line(self,line):
         dict = {'attitude': Attitude , 'direct-subjective':DS , 'agent':Agent, 'expressive-subjectivity':SE,'objective-speech-event':OSE ,'sentence':Sentence,'targetFrame':TargetFrame, 'eTarget':ETarget, 'sTarget':STarget}
@@ -189,49 +228,54 @@ class MPQA:
         id = annotation_line[0]
         span = annotation_line[1]
         anno_type = annotation_line[2]
-        #print(id,span,anno_type, len(annotation_line) )
-
         kwargs={}
         kwargs['annotation_id']= id
         kwargs['span']= span
 
         attributes = annotation_line[3]
         attributes = attributes.replace('-','_')
-        attributes = attributes.split(' ')
-        attributes = attributes[:-1]
+
+        attributes = attributes.split(' ') # this wont work for some attitude cases
+        attributes = attributes[:-1] # to deal with an empty element that was appearing
         for attr in attributes:
             #print('attr',attr)
-            key_val = attr.split('=')
-            #print('key_val',key_val)
-            try:
-                kwargs[key_val[0]] = key_val[1]
-            except IndexError:
-                print('anno_type',anno_type)
-                print('attributes',attributes)
-                print('IndexError',key_val)
-                print('attr',attr)
+            if('=' in attr):
+                key_val = attr.split('=')
+                try:
+                    kwargs[key_val[0]] = key_val[1]
+                except IndexError:
+                    print('anno_type', anno_type)
+                    print('attributes', attributes)
+                    print('IndexError', key_val)
+                    print('attr', attr)
+                finally:
+                    #print(key_val[0], '=', key_val[1])
+                    pass
+            else:
 
-            finally:
                 pass
+            #print('key_val',key_val)
         try:
             #print(kwargs)
+            #print('anno_type',anno_type)
             b = dict[anno_type](**kwargs)
+            if(anno_type=='direct-subjective'):
+                print(dict[anno_type],kwargs)
+            #print(anno_type,dict[anno_type],'kwargs:',kwargs)
+            return b
         except KeyError:
             #print('dict error', anno_type)
             #print('annotation_line',annotation_line)
             pass
         except TypeError:
             pass
-            #print('TypeError',kwargs )
+            #print('TypeError',kwargs)
         finally:
+            print('finally',anno_type)
             pass
-
-
-
 
     def getFullPath(self, doc_type, doc_path):
         return str(self.path + '/' + doc_type + '/' + doc_path)
-
 
     def list_files(self, corpus):
         files = corpus.fileids()
@@ -243,5 +287,4 @@ class MPQA:
 print('1:config step..')
 config = Config([MPQA.name])
 config.run()
-
 mpqa = MPQA(config.data_path, config.getCorpusPath(config.corpusList[0]))
